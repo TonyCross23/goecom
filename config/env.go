44e95275cab1899b656d965ce348fbaef5deb51f
TonyCross23 <tonychamp40@gmail.com>
2025-09-25 19:18:35 +0630

    db

--- config/env.go
+++ config/env.go
@@ -25,7 +25,7 @@ func InitConfig() Config {
 		PublicHost: getEnv("PUBLIC_HOST", "http://localhost"),
 		Port:       getEnv("PORT", "8080"),
 		DBUser:     getEnv("DB_USER", "root"),
-		DBPassword: getEnv("DB_PASSWORD", "mypassword"),
+		DBPassword: getEnv("DB_PASSWORD", "password123"),
 		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
 		DBName:     getEnv("DB_NAME", "ecom"),
 	}
