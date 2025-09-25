8eb5c46b772b683e0d911fa8b8c8b4ae65d3dfee
TonyCross23 <tonychamp40@gmail.com>
2025-09-25 13:56:18 +0630

    db

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/TonyCross23/goecom/cmd/api"
+	"github.com/TonyCross23/goecom/config"
+	"github.com/TonyCross23/goecom/database"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewApiServer(":8080", nil)
+	db, err := database.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	InitStorage(db)
+
+	server := api.NewApiServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func InitStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal("Failed to ping database:", err)
+	}
+	log.Println("Connected to the database successfully")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/TonyCross23/goecom
 
 go 1.24.2
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lpernett/godotenv v0.0.0-20230527005122-0de1d4c5ef5e // indirect
+)
